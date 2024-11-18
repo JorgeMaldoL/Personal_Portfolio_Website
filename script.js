356b358afe9599f12857dae3049ca4274f3d804b
jorgemaldonado <maldonadoj747@gmail.com>
2024-11-18 01:17:33 -0500

    Added dark mode

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 const navLinkEls = document.querySelectorAll('.nav_link');
 const sectionEls = document.querySelectorAll('.section');
+const darkModeSwitch = document.getElementById('darkModeSwitch');
 
 let currentSection = 'home';
 window.addEventListener('scroll', () => {
-    sectionEls.forEach(sectionEl =>{
-        if(window.scrollY >= (sectionEl.offsetTop - sectionEl.clientHeight/ 2)) {
+    sectionEls.forEach(sectionEl => {
+        if (window.scrollY >= (sectionEl.offsetTop - sectionEl.clientHeight / 2)) {
             currentSection = sectionEl.id;
         }
     });
 
-    navLinkEls.forEach(navLinkEl =>{
-        if (navLinkEl.href.includes(currentSection)){
+    navLinkEls.forEach(navLinkEl => {
+        if (navLinkEl.href.includes(currentSection)) {
             document.querySelector('.active').classList.remove('active');
             navLinkEl.classList.add('active');
         }
     });
 });
+
+if (darkModeSwitch) {
+    darkModeSwitch.addEventListener('change', () => {
+        document.body.classList.toggle('dark-mode');
+    });
+}
